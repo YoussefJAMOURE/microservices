d3abab3231718cfd581b431cc5a43ff03d6c1f3e
YoussefJAMOURE <y.jamoure@gmail.com>
2019-08-26 12:40:10 +0100

    Added individual config files

---
+++ CentralConfig/src/main/java/io/microservices/centralConfig/CentralConfigApplication.java
@@ -0,0 +1,15 @@
+package io.microservices.centralConfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class CentralConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralConfigApplication.class, args);
+	}
+
+}
